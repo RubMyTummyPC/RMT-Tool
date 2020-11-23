e8f26247df91bf291da9516d9827393ee59b7ec0
RubMyTummyPC <74920334+RubMyTummyPC@users.noreply.github.com>
2020-11-23 16:36:18 +0100

    Add files via upload

---
+++ RMT-Tool/tools/addons/logo.py
@@ -0,0 +1,10 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+ ___      _    __  __        _  _   _           _   
+| _ \_  _| |__|  \/  |_  _  /_\| |_| |_ __ _ __| |__
+|   / || | '_ \ |\/| | || |/ _ \  _|  _/ _` / _| / /
+|_|_\\_,_|_.__/_|  |_|\_, /_/ \_\__|\__\__,_\__|_\_\
+                                    Created By RubMyTummy{Fore.RESET}""")
